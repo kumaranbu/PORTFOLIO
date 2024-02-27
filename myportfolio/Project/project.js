66720c1cc4ea59ab3c94f3810496c3fd7493ed78
kumaranbu <kumaranbu283@gmail.com>
2024-02-27 18:59:50 +0530

    Projects folder added

---
+++ myportfolio/Project/project.js
@@ -0,0 +1,32 @@
+function validateForm() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    // Simple validation rules
+
+    if (username === '' || password === '') {
+         alert('Please fill in all fields');
+          return false;
+}
+   
+    return true;
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+    
+
+    
+    
+    
